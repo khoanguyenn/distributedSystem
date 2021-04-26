1b47c476e92d1ccbe22d821cc4435e7d76342213
khoanguyenn <44629203+khoanguyenn@users.noreply.github.com>
2021-04-27 00:23:48 +0700

    update: advertising exercise

---
+++ src/main/java/AdvertisingExercise/Student.java
@@ -0,0 +1,35 @@
+package AdvertisingExercise;
+
+import java.io.Serializable;
+
+public final class Student implements Serializable {
+	private int id;
+	private String fullName;
+	private String year;
+
+	Student(int id, String fullName, String year) {
+		this.id = id;
+		this.fullName = fullName;
+		this.year = year;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public String getYear() {
+		return year;
+	}
+}
